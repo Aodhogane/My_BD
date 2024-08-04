1af9630f641a5678cb476834285315ad82dbaebc
Илья <152794862+Aodhogane@users.noreply.github.com>
2024-08-04 18:10:43 +0300

    -

--- src/main/java/com/example/my_bd_spring/repositories/CustomAnimalRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.my_bd_spring.repositories;
-
-public interface CustomAnimalRepository {
-    Integer addNewAnimalAndAssign(String species, Integer classId, Integer zooId, Integer cellId, Integer workerId);
-}
