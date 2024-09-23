92fed7cf3a940901d668b200ef886f8865d88880
Илья <152794862+Aodhogane@users.noreply.github.com>
2024-09-23 13:51:05 +0300

    Ограниче для "AnimalRepositoryContract"

--- src/main/java/com/example/my_bd_spring/contract/AnimalRepositoryContract.java
+++ src/main/java/com/example/my_bd_spring/contract/AnimalRepositoryContract.java
@@ -2,4 +2,10 @@ package com.example.my_bd_spring.contract;
 
 public interface AnimalRepositoryContract {
     Integer addNewAnimalAndAssign(String species, Integer classId, Integer zooId, Integer cellId, Integer workerId);
+
+    default Integer addNewAnimalAndAssignWithRestrictions(String species, Integer classId, Integer zooId, 
+    Integer cellId, Integer workerId) {
+        // реализация метода с ограничениями
+        return null; // или любое другое значение типа Integer
+    }
 }
