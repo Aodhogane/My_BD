58b02136be18d7117779056b2cfc0bcd78186c47
Илья <152794862+Aodhogane@users.noreply.github.com>
2024-09-26 02:29:05 +0300

    Реализация ограничения для AnimalRepositoryContract

--- src/main/java/com/example/my_bd_spring/contract/AnimalRepositoryContract.java
+++ src/main/java/com/example/my_bd_spring/contract/AnimalRepositoryContract.java
@@ -3,8 +3,8 @@ package com.example.my_bd_spring.contract;
 public interface AnimalRepositoryContract {
     Integer addNewAnimalAndAssign(String species, Integer classId, Integer zooId, Integer cellId, Integer workerId);
 
-    default Integer addNewAnimalAndAssignWithRestrictions(String species, Integer classId, Integer zooId, 
+    default Integer RestrictionsforAnimals(String species, Integer classId, Integer zooId, 
     Integer cellId, Integer workerId) {  // реализация метода с ограничениями 
-        return null;
+        return 1;
     }
 }
